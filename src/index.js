aefdc1238a62faef0eabc2b2345347e6db49a75e
Olga <70957483+Lola-1992@users.noreply.github.com>
2020-10-14 22:28:21 +0300

    refactor:
    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix === undefined || matrix === []) {
+  return [] };
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+     if (i % 2 !== 0) {
+          matrix[i].sort((a , b) => b - a );
+   } else {
+          matrix[i].sort((a , b) => a - b );
+   }
+          matrix[i].forEach(item => arr.push(item));
+   }
+    return arr;
+   };
